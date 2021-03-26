fedecc24979238145a7325d8ebf2e11a7e234809
yuikim <nokdoo00@gmail.com>
2021-03-26 16:01:52 -0400

    add Checkedbox

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
 import SaveIcon from '@material-ui/icons/Save';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Checkbox from '@material-ui/core/Checkbox';
+
+function CheckboxExample() {
+  const [checked, setChecked] = useState(true)
+  return(
+    <div>
+      <Checkbox
+        checked={checked}
+        onChange={(e) => setChecked(e.target.checked)}
+      />
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <CheckboxExample />
+      <ButtonGroup variant="contained" color="primary">
         <Button 
-          startIcon={<SaveIcon/>}
+/*           startIcon={<SaveIcon/>} */
+          endIcon={<SaveIcon/>}
           size="large"
           style={{
             fontsize: 15
           }}
           herf="#"
-          onClick={() => alert('test')} 
-          variant="contained" 
-          color="secondary"
+          onClick={() => alert('test')}   
 /*           disabled */
         >
-          Hello World
+          Save
         </Button>
+        <Button 
+          endIcon={<DeleteIcon/>}
+          size="large"
+          style={{
+            fontsize: 15
+          }}
+          color="secondary"
+        >
+          Discard
+        </Button>
+      </ButtonGroup>
+        
       </header>
     </div>
   );
