e49386ec2380d41953ce5450104ff527a084871b
yuikim <nokdoo00@gmail.com>
2021-03-26 15:35:42 -0400

    add icon in button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Button 
+          startIcon={<SaveIcon/>}
           size="large"
           style={{
             fontsize: 15
