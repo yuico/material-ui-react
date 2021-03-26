b54ccbed79474ae44d44ea596ed9dc6e64513c3f
yuikim <nokdoo00@gmail.com>
2021-03-26 18:18:08 -0400

    use FormControlLable

--- src/App.js
+++ src/App.js
@@ -5,16 +5,22 @@ import Button from '@material-ui/core/Button';
 import SaveIcon from '@material-ui/icons/Save';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 function CheckboxExample() {
   const [checked, setChecked] = useState(true)
   return(
-    <div>
-      <Checkbox
+    <FormControlLabel 
+      control={<Checkbox
         checked={checked}
         onChange={(e) => setChecked(e.target.checked)}
-      />
-    </div>
+        color="primary"
+        inputProps={{
+          'aria-label' : 'secondary checkbox' 
+        }}
+      />}
+      label="Testing Checkbox"
+    />
   )
 }
 
